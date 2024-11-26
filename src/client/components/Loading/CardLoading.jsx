08a64348abc144d323aff8f57ea13cf8dec4aae6
Kawser-WD <kawser.wd@gmail.com>
2024-11-26 16:25:18 +0600

    progress issue solved

--- src/client/components/Loading/CardLoading.jsx
+++ src/client/components/Loading/CardLoading.jsx
@@ -4,6 +4,7 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Progress,
   Typography,
 } from "@material-tailwind/react";
 
@@ -44,7 +45,7 @@ const CardLoading = () => {
                 d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
               />
             </svg>
-            <Typography className="text-sm font-normal"></Typography>
+            <Typography className="text-sm font-normal">&nbsp;</Typography>
           </div>
           <Button className="bg-primary hover:bg-secondary duration-200 ease-in text-text-primary rounded-none uppercase">
             &nbsp;
